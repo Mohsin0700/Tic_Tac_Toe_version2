2e69be60fd8b48fdd9e4eef9a8a0d6aa6b7e9b66
Mohsin Hussain <muhammad786mohsin024@gmail.com>
2022-10-30 22:07:24 -0800

    Update 2.1
    Description will be added soon

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let c6 = document.getElementById("c6");
 let c7 = document.getElementById("c7");
 let c8 = document.getElementById("c8");
 let c9 = document.getElementById("c9");
+let allC = [c1,c2,c3,c4,c5,c6,c7,c8,c9];
 let turn = 1;
 let myStatus = document.getElementById("status");
 let p1Score = document.getElementById("p1");
@@ -15,15 +16,18 @@ let draw = document.getElementById("d");
 let p1 = 0;
 let p2 = 0;
 let d = 0;
+let at = 0;
 
 // Main Function....
 const mainFunc = (x) => {
     // Player Condition...
     if(turn === 1) {
+        document.getElementById(x).classList.replace('btn-info', 'btn-success');
         document.getElementById(x).innerHTML = 'O';
         turn = 2;
         document.getElementById("turn").innerHTML = 'Player 2';
     } else if (turn === 2) {
+        document.getElementById(x).classList.replace('btn-info', 'btn-danger');
         document.getElementById(x).innerHTML = 'X';
         turn = 1;
         document.getElementById("turn").innerHTML = 'Player 1';
@@ -34,10 +38,16 @@ const mainFunc = (x) => {
         myStatus.innerHTML = `Player 1 Win !`;
         p1++;
         p1Score.innerHTML = `&nbsp ${p1}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c4.innerHTML === 'O' && c5.innerHTML === 'O' && c6.innerHTML === 'O') {
         myStatus.innerHTML = `Player 1 Win !`;
         p1++;
         p1Score.innerHTML = `&nbsp ${p1}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c7.innerHTML === 'O' && c8.innerHTML === 'O' && c9.innerHTML === 'O') {
         myStatus.innerHTML = `Player 1 Win !`;
         p1++;
@@ -50,18 +60,30 @@ const mainFunc = (x) => {
         myStatus.innerHTML = `Player 1 Win !`;
         p1++;
         p1Score.innerHTML = `&nbsp ${p1}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c3.innerHTML === 'O' && c5.innerHTML === 'O' && c7.innerHTML === 'O') {
         myStatus.innerHTML = `Player 1 Win !`;
         p1++;
         p1Score.innerHTML = `&nbsp ${p1}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c1.innerHTML === 'O' && c4.innerHTML === 'O' && c7.innerHTML === 'O') {
         myStatus.innerHTML = `Player 1 Win !`; 
         p1++;
         p1Score.innerHTML = `&nbsp ${p1}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c3.innerHTML === 'O' && c6.innerHTML === 'O' && c9.innerHTML === 'O') {
         myStatus.innerHTML = `Player 1 Win !`; 
         p1++;
         p1Score.innerHTML = `&nbsp ${p1}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     }
 
     // Functionality for x
@@ -69,34 +91,58 @@ const mainFunc = (x) => {
         myStatus.innerHTML = 'Player 2 Win !';
         p2++;
         p2Score.innerHTML = `&nbsp ${p2}`; 
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c4.innerHTML === 'X' && c5.innerHTML === 'X' && c6.innerHTML === 'X') {
         myStatus.innerHTML = 'Player 2 Win !';
         p2++;
         p2Score.innerHTML = `&nbsp ${p2}`; 
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c7.innerHTML === 'X' && c8.innerHTML === 'X' && c9.innerHTML === 'X') {
         myStatus.innerHTML = 'Player 2 Win !';
         p2++;
         p2Score.innerHTML = `&nbsp ${p2}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c1.innerHTML === 'X' && c5.innerHTML === 'X' && c9.innerHTML === 'X') {
         myStatus.innerHTML = 'Player 2 Win !';
         p2++;
         p2Score.innerHTML = `&nbsp ${p2}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c2.innerHTML === 'X' && c5.innerHTML === 'X' && c8.innerHTML === 'X') {
         myStatus.innerHTML = 'Player 2 Win !';
         p2++;
         p2Score.innerHTML = `&nbsp ${p2}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c3.innerHTML === 'X' && c5.innerHTML === 'X' && c7.innerHTML === 'X') {
         myStatus.innerHTML = 'Player 2 Win !';
         p2++;
         p2Score.innerHTML = `&nbsp ${p2}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c1.innerHTML === 'X' && c4.innerHTML === 'X' && c7.innerHTML === 'X') {
         myStatus.innerHTML = 'Player 2 Win !';
         p2++;
         p2Score.innerHTML = `&nbsp ${p2}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     } else if (c3.innerHTML === 'X' && c6.innerHTML === 'X' && c9.innerHTML === 'X') {
         myStatus.innerHTML = 'Player 2 Win !';
         p2++;
         p2Score.innerHTML = `&nbsp ${p2}`;
+        for (let i = 0; i < allC.length; i++) {
+            allC[i].setAttribute('disabled','');
+        }
     }
     // Draw Condition...
     if (c1.innerText !== '' && c2.innerText !== '' && c3.innerText !== '' && c4.innerText !== '' && c5.innerText !== '' && c6.innerText !== '' && c7.innerText !== '' && c8.innerText !== '' && c9.innerText !== '' && myStatus.innerHTML === '') {
@@ -104,6 +150,7 @@ const mainFunc = (x) => {
         d++;
         draw.innerHTML = `&nbsp ${d}`;
     }
+    document.getElementById(x).setAttribute('disabled','');
 }
 
 const playAgain = () => {
@@ -118,4 +165,33 @@ const playAgain = () => {
     c9.innerHTML = '';
     turn = 1;
     myStatus.innerHTML = '';
+    c1.classList.replace('btn-success', 'btn-info');
+    c2.classList.replace('btn-success', 'btn-info');
+    c3.classList.replace('btn-success', 'btn-info');
+    c4.classList.replace('btn-success', 'btn-info');
+    c5.classList.replace('btn-success', 'btn-info');
+    c6.classList.replace('btn-success', 'btn-info');
+    c7.classList.replace('btn-success', 'btn-info');
+    c8.classList.replace('btn-success', 'btn-info');
+    c9.classList.replace('btn-success', 'btn-info');
+    
+    c1.classList.replace('btndanger', 'btn-info');
+    c2.classList.replace('btn-danger', 'btn-info');
+    c3.classList.replace('btn-danger', 'btn-info');
+    c4.classList.replace('btn-danger', 'btn-info');
+    c5.classList.replace('btn-danger', 'btn-info');
+    c6.classList.replace('btn-danger', 'btn-info');
+    c7.classList.replace('btn-danger', 'btn-info');
+    c8.classList.replace('btn-danger', 'btn-info');
+    c9.classList.replace('btn-danger', 'btn-info');
+
+    c1.removeAttribute('disabled');
+    c2.removeAttribute('disabled');
+    c3.removeAttribute('disabled');
+    c4.removeAttribute('disabled');
+    c5.removeAttribute('disabled');
+    c6.removeAttribute('disabled');
+    c7.removeAttribute('disabled');
+    c8.removeAttribute('disabled');
+    c9.removeAttribute('disabled');
 }
